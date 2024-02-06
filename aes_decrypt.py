e4086772cc46b5bc2538509d4831b42c3934c5c1
soapgu <ghost_gu@163.com>
2024-02-06 10:39:21 +0800

    feat: 🎸 增加耗时日志

--- aes_decrypt.py
+++ aes_decrypt.py
@@ -3,12 +3,13 @@ from Crypto.Util.Padding import unpad
 import binascii
 import os
 import argparse
+import time
 
 parser = argparse.ArgumentParser(description='Process some files.')
 parser.add_argument('-f', '--input-file', required=True, help='The input file')
 args = parser.parse_args()
 print(f"Input file: {args.input_file}")
-
+start_time = time.time()
 keys = '30980f98296b77f00a55f3c92b35322d898ae2ffcdb906de40336d2cf3d556a0'
 iv = 'e5889166bb98ba01e1a6bc9b32dbf3e6'
 download_path = args.input_file
@@ -30,12 +31,14 @@ with open(download_path, 'rb') as in_file:
             if(block_count == count):
                 print(f"before pad content:{decrypted_chunk}")
                 decrypted_chunk = unpad( decrypted_chunk,AES.block_size )
-            hex_string = binascii.hexlify(decrypted_chunk).decode()
-            print(f"content:{hex_string}")
+            #hex_string = binascii.hexlify(decrypted_chunk).decode()
+            #print(f"content:{hex_string}")
             out_file.write(decrypted_chunk)
-            print(f"计数{count}/总数{block_count},length:{len(decrypted_chunk)}")
-
+            #print(f"计数{count}/总数{block_count},length:{len(decrypted_chunk)}")
 print(f"decrypt {output_filename} OK")
 
 out_file_length = os.path.getsize(output_filename)
 print(f"in file size:{in_file_length},out file size{out_file_length}")
+end_time = time.time()
+run_time = end_time - start_time
+print(f"AES解密运行时间: {run_time} 秒")
--- aes_encrypt.py
+++ aes_encrypt.py
@@ -3,6 +3,7 @@ from Crypto.Util.Padding import pad,unpad
 import binascii
 import os
 import argparse
+import time
 
 
 parser = argparse.ArgumentParser(description='Process some files.')
@@ -23,18 +24,25 @@ cipher = AES.new(binascii.unhexlify(keys), AES.MODE_CBC,iv=binascii.unhexlify(iv
 in_file_length = os.path.getsize(download_path)
 block_count = in_file_length / AES.block_size
 print(in_file_length)
-
+start_time = time.time()
 count = 0
 with open(download_path, 'rb') as in_file:
     with open(output_filename, 'wb') as out_file:
-        while content := in_file.read(AES.block_size):
-            if(len(content) < AES.block_size):
+        while True:
+            content = in_file.read(AES.block_size)
+            end = len(content) < AES.block_size
+            if(end):
                 content = pad(content,AES.block_size)
                 hex_string = binascii.hexlify(content).decode()
                 print(f"pad content:{hex_string}")
             crypted_chunk = cipher.encrypt(content)
             out_file.write(crypted_chunk)
+            if(end):
+                break
             #print(f"计数{count}/总数{block_count},length:{len(crypted_chunk)}")
 
 out_file_length = os.path.getsize(output_filename)
 print(f"encrypt ok,size {out_file_length}")
+end_time = time.time()
+run_time = end_time - start_time
+print(f"AES加密运行时间: {run_time} 秒")
